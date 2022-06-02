928dd30d6a6d19a562036e51cd45d5a15408fff3
Muhammed Tariq <momotrq94@gmail.com>
2022-06-01 20:01:02 -0400

    add feature dashboard view

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'].to_s, password: ENV['PASSWORD'].to_s
   def show
+    @products = Product.count
+    @categories = Category.count
   end
 end
